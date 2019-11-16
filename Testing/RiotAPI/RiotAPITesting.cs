749961a0de43db3ceb226884bc25a9c88efa7047
Milkenm <miguel.campos.johnny@gmail.com>
2019-11-16 12:46:10 +0000

    RiotAPI: Get most mastery

--- Testing/RiotAPI/RiotAPITesting.cs
+++ Testing/RiotAPI/RiotAPITesting.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Text;
@@ -11,11 +12,33 @@ namespace Testing.RiotAPI
 	public partial class RiotAPITesting : Form
 	{
 		Settings set = new Settings();
-		string url = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{0}?api_key={1}";
+		string sumInfo = "https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{0}?api_key={1}";
+		string masteryInfo = "https://euw1.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/{0}?api_key={1}";
+
+
+
+
 
 		public RiotAPITesting() => InitializeComponent();
-		private void RiotAPI_Load(object sender, EventArgs e) => textBox_apiKey.Text = set.ApiKey;
+
+		private void RiotAPI_Load(object sender, EventArgs e)
+		{
+			textBox_summoner.Text = set.SummonerName;
+			textBox_apiKey.Text = set.ApiKey;
+		}
+
 		private void RiotAPITesting_FormClosing(object sender, FormClosingEventArgs e) => set.Save();
+
+
+
+
+
+
+
+
+
+
+
 
 
 		public class Summoner
@@ -28,6 +51,20 @@ namespace Testing.RiotAPI
 			public string revisionDate { get; set; }
 			public string summonerLevel { get; set; }
 		}
+		
+		public class Mastery
+		{
+			public string championLevel { get; set; }
+			public string chestGranted { get; set; }
+			public string championPoints { get; set; }
+			public string championPointsSinceLastLevel { get; set; }
+			public string championPointsUntilNextLevel { get; set; }
+			public string summonerId { get; set; }
+			public string tokensEarned { get; set; }
+			public string championId { get; set; }
+			public string lastPlayTime { get; set; }
+		}
+
 
 
 		private void button_saveApiKey_Click(object sender, EventArgs e) => set.ApiKey = textBox_apiKey.Text;
@@ -50,14 +87,19 @@ namespace Testing.RiotAPI
 				textBox_revisionDate.Text = sum.revisionDate;
 				textBox_summonerLevel.Text = sum.summonerLevel;
 
-				pictureBox_icon.Load(string.Format("https://milkenm.github.io/Diamond/Static/LoL/Icons/{0}.png", textBox_profileIconid.Text));
+				//pictureBox_icon.Load(string.Format("https://milkenm.github.io/Diamond/Static/LoL/Icons/{0}.png", textBox_profileIconid.Text));
+
+				
+				string json2 = GET(string.Format(masteryInfo, textBox_id.Text, textBox_apiKey.Text));
+				List<Mastery> mastery = JsonConvert.DeserializeObject<List<Mastery>>(json2);
+				MessageBox.Show(mastery[0].championId, "Most Points");
 			}
 			else MessageBox.Show("Esse Summoner não foi encontrado.");
 		}
 
 		string FormatString()
 		{
-			return string.Format(url, textBox_summoner.Text, textBox_apiKey.Text);
+			return string.Format(sumInfo, textBox_summoner.Text.Replace(" ", "%20"), textBox_apiKey.Text);
 		}
 
 		public string GET(string url)
