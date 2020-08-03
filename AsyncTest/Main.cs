c01c5b031572e7aec23f7e61e85dbe1f7ef0a378
Milkenm <miguel.campos.johnny@gmail.com>
2020-08-03 03:26:18 +0100

    Async Testing

---
+++ AsyncTest/Main.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncTest
+{
+    public partial class Main : Form
+    {
+        private readonly int Delay = 1000;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        // ASYNC
+        private async void button_runTaskAsync_Click(object sender, EventArgs e)
+        {
+            AsyncTask();
+
+            MessageBox.Show("Completed");
+        }
+        
+        private async Task AsyncTask()
+        {
+            Thread.Sleep(Delay);
+        }
+
+        // NORMAL
+        private void button_runTask_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("lmao");
+            string res = NormalTask();
+
+            MessageBox.Show(res);
+        }
+
+        private string NormalTask()
+        {
+            Thread.Sleep(Delay);
+            return "Task Complete!";
+        }
+    }
+}
---
+++ AsyncTestConsole/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTestConsole
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            ExecuteJob();
+            ExecuteJobAsync();
+
+            Console.ReadKey();
+        }
+
+        private static void Job()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine(" » " + i);
+            }
+        }
+
+        private static Task JobAsync()
+        {
+            return Task.Run(() => Job());
+        }
+
+        private static void ExecuteJob()
+        {
+            JobAsync();
+            Console.WriteLine("Job Done!");
+        }
+
+        private static async void ExecuteJobAsync()
+        {
+            await JobAsync();
+            Console.WriteLine("Async Job Done!");
+        }
+    }
+}
