e38a6300fc96ca5062b107333a905764dff142d8
Milkenm <miguel.campos.johnny@gmail.com>
2020-08-08 22:03:37 +0100

    Dynamic or Object or idk test yes

---
+++ DynamicDataTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+
+namespace DynamicDataTable
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Value", typeof(object));
+
+            dt.PrimaryKey = new DataColumn[] { dt.Columns["Value"] };
+
+            dt.Rows.Add(1);
+            dt.Rows.Add("Text");
+
+            if (dt.Rows.Contains("Text"))
+            {
+                Console.WriteLine("Contains");
+            }
+            else
+            {
+                Console.WriteLine("Does not contain");
+            }
+        }
+    }
+}
