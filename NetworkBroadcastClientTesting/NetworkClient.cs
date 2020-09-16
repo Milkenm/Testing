3c0f406b0908aedd4fab31d287e904d6450a1e4b
Milkenm <miguel.campos.johnny@gmail.com>
2020-09-16 17:02:22 +0100

    Network Broadcast Testing

---
+++ NetworkBroadcastClientTesting/NetworkClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Net;
+
+using static ScriptsLib.Network.Packets;
+
+namespace NetworkBroadcastClientTesting
+{
+    internal static class NetworkClient
+    {
+        private static void Main()
+        {
+            SendUdpPacket(IPAddress.Broadcast.ToString(), 11000, "lel");
+            Console.WriteLine("Message sent to the broadcast address");
+        }
+    }
+}
---
+++ NetworkBroadcastHostTesting/NetworkHost.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+
+using static ScriptsLib.Network.Packets;
+
+namespace NetworkBroadcast.HostTesting
+{
+    internal static class NetworkHost
+    {
+        public static void Main()
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast");
+
+                    string message = WaitUdpPacket(11000);
+
+                    Console.WriteLine($"Received: {message}");
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
