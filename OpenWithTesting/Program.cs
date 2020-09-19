6d03e243d9de1a276b3ad2c64becfff43512e962
Milkenm <miguel.campos.johnny@gmail.com>
2020-09-19 20:47:33 +0100

    OpenWith Test

---
+++ OpenWithTesting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace OpenWithTesting
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+        }
+    }
+}
