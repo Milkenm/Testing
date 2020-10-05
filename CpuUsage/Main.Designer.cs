b7381bef94c65144f50cf99fba53c6db06e68118
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-05 23:45:31 +0100

    CPU Usage Test

---
+++ CpuUsage/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace CpuUsage
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.SuspendLayout();
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(15, 14);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(311, 420);
+			this.listBox1.TabIndex = 0;
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(340, 448);
+			this.Controls.Add(this.listBox1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Main";
+			this.Text = "CPU Usage";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListBox listBox1;
+	}
+}
+
---
+++ CpuUsage/Main.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CpuUsage
+{
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+
+			Info();
+			LoadCPU();
+			LoadCPU();
+			LoadCPU();
+			LoadCPU();
+			LoadCPU();
+		}
+
+		public async Task Info()
+		{
+			// https://stackoverflow.com/a/35289332
+			await Task.Run(() =>
+			{
+				PerformanceCounter process_cpu = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+				while (true)
+				{
+					Thread.Sleep(500);
+					float processUsage = process_cpu.NextValue() / Environment.ProcessorCount;
+					listBox1.Items.Add(processUsage);
+					listBox1.SelectedIndex = listBox1.Items.Count - 1;
+				}
+			}).ConfigureAwait(false);
+			/*
+			await Task.Run(() =>
+			{
+				// CPU time (or process time) is the amount of time for which a central processing unit (CPU) was used for processing instructions of a computer program or operating system
+				PerformanceCounter cpuCounter = new PerformanceCounter("Process", "% Processor Time", Process.GetCurrentProcess().ProcessName);
+				for (int i64 = 0; i64 < 100; ++i64)
+				{
+					Thread.Sleep(1000);
+					listBox1.Items.Add("CPU: " + (cpuCounter.NextValue() / 4) + " %");
+				}
+			}).ConfigureAwait(false);
+			*/
+		}
+
+		public async Task LoadCPU()
+		{
+			await Task.Run(() =>
+			{
+				int a = 10;
+
+				for (int i = 0; ; i++)
+				{
+					a += 19028312 * 3 * 12;
+				}
+			}).ConfigureAwait(false);
+		}
+	}
+}
