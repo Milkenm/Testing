f87bed95b02461cb1fd7896ec8353d5bc1639947
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-09 19:57:49 +0100

    Unity 2D UFO Tutorial
    Amazing tutorial btw

---
+++ UnityTesting/2D UFO Tutorial/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+	public GameObject player;
+
+	private Vector3 offset;
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		offset = transform.position - player.transform.position;
+	}
+
+	// Update is called once per frame
+	private void LateUpdate()
+	{
+		transform.position = player.transform.position + offset;
+	}
+}
