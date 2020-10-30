2e00c5fd630003084428467bf6b70d7078e0879a
Milkenm <miguel.campos.johnny@gmail.com>
2020-10-30 10:38:51 +0000

    FormResize testing

---
+++ FormResizeTesting/Main.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace FormResizeTesting
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+			this.button_prevent = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button_prevent
+			// 
+			this.button_prevent.Location = new System.Drawing.Point(212, 92);
+			this.button_prevent.Name = "button_prevent";
+			this.button_prevent.Size = new System.Drawing.Size(126, 66);
+			this.button_prevent.TabIndex = 0;
+			this.button_prevent.Text = "Prevent";
+			this.button_prevent.UseVisualStyleBackColor = true;
+			this.button_prevent.Click += new System.EventHandler(this.PreventResize);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(551, 250);
+			this.Controls.Add(this.button_prevent);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Main";
+			this.Text = "Form Resize Testing";
+			this.Load += new System.EventHandler(this.Main_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button_prevent;
+	}
+}
+
---
+++ FormResizeTesting/Main.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace FormResizeTesting
+{
+	public partial class Main : Form
+	{
+		public Main()
+		{
+			InitializeComponent();
+		}
+
+		private void Main_Load(object sender, EventArgs e)
+		{
+			PreventResize(null, null);
+		}
+
+		private static bool preventResize;
+
+		private void PreventResize(object sender, EventArgs e)
+		{
+			if (preventResize)
+			{
+				preventResize = false;
+				MinimumSize = Size;
+				MaximumSize = Size;
+			}
+			else
+			{
+				preventResize = true;
+				MinimumSize = new Size(0, 0);
+				MaximumSize = new Size(0, 0);
+			}
+		}
+	}
+}
