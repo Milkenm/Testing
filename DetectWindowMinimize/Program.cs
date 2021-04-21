9171475e606f058c8868cbd1259a3fc1f4c04c49
Milkenm <miguel.campos.johnny@gmail.com>
2021-04-21 03:19:22 +0100

    DetectWindowMinimize ❌

---
+++ DetectWindowMinimize/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace DetectWindowMinimize
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Process[] processes = Process.GetProcessesByName("githubdesktop");
+			if (processes.Length > 0)
+			{
+				Process p = processes[0];
+				IntPtr wHnd = p.MainWindowHandle;
+				Console.WriteLine("Minimized: " + IsIconic(wHnd));
+			}
+			else
+			{
+				Console.WriteLine("No processes found.");
+			}
+
+			Console.ReadLine();
+		}
+
+		[DllImport("user32.dll")]
+		[return: MarshalAs(UnmanagedType.Bool)]
+		static extern bool IsIconic(IntPtr hWnd);
+	}
+}
