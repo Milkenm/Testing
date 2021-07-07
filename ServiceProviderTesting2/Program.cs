618753e9e049679ba93279d4cce64288967295cf
Milkenm <miguel.campos.johnny@gmail.com>
2021-07-07 10:03:34 +0100

    ServiceProviderTesting2

---
+++ ServiceProviderTesting2/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+
+namespace ServiceProviderTesting2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceCollection sc = new ServiceCollection();
+			sc.AddSingleton(new service("lmao"));
+			ServiceProvider sp = sc.BuildServiceProvider();
+
+
+			var a = new a(sp.GetRequiredService<service>());
+			a.print();
+
+			Console.WriteLine(sp.GetService<service>().kek());
+		}
+
+		class a
+		{
+			private service _service;
+
+			public a(service sv)
+			{
+				_service = sv;
+			}
+
+			public void print()
+			{
+				Console.WriteLine(_service.kek());
+			}
+		}
+
+		interface iservice
+		{
+			public string kek();
+		}
+
+		class service : iservice
+		{
+			string msg;
+
+			public service(string msg)
+			{
+				this.msg = msg;
+			}
+
+			public string kek()
+			{
+				return this.msg;
+			}
+		}
+	}
+}
