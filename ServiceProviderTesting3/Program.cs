d955914e29a3825a65fb315ad616de4f593e2089
Milkenm <miguel.campos.johnny@gmail.com>
2021-07-09 10:22:59 +0100

    ServiceProviderTesting3

---
+++ ServiceProviderTesting3/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+
+namespace ServiceProviderTesting3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ServiceCollection sc = new ServiceCollection();
+			sc.AddSingleton<Dictionary<string, string>>();
+
+			ServiceProvider sp = sc.BuildServiceProvider();
+			Dictionary<string,string> dc = sp.GetService<Dictionary<string, string>>();
+			dc.Add("1", "sdfsd2");
+
+			Dictionary<string, string> dc2 = sp.GetService<Dictionary<string, string>>();
+			Console.WriteLine(dc2["1"]);
+		}
+	}
+}
