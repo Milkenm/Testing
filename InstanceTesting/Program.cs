69c4150ed5755f937d6cdb7be299e563d96350f6
Milkenm <miguel.campos.johnny@gmail.com>
2022-05-20 10:52:01 +0100

    Instance testing ✅

---
+++ InstanceTesting/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InstanceTesting
+{
+	internal class Program
+	{
+		private static readonly TestClass TEST_CLASS = TestClass.GetInstance();
+
+		private static void Main(string[] args)
+		{
+			int value = TEST_CLASS.GetValue();
+			Console.WriteLine($"Class value: {value}.");
+		}
+	}
+}
---
+++ InstanceTesting/TestClass.cs
@@ -0,0 +1,26 @@
+﻿namespace InstanceTesting
+{
+	internal class TestClass
+	{
+		private static TestClass Instance;
+
+		public static TestClass GetInstance()
+		{
+			return Instance ?? new TestClass();
+		}
+
+		public TestClass()
+		{
+			Instance = this;
+
+			value = 5;
+		}
+
+		private int value;
+
+		public int GetValue()
+		{
+			return value;
+		}
+	}
+}
