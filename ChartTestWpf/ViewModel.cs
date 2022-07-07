9224325643200f6698129b0c58bfe4e59d536b9a
Milkenm <miguel.campos.johnny@gmail.com>
2022-07-07 23:56:48 +0100

    Chart testing

---
+++ ChartTestWpf/ViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore;
+
+namespace ChartTestWpf
+{
+    public class ViewModel
+    {
+        public ISeries[] Series { get; set; }
+            = new ISeries[]
+            {
+                new LineSeries<double>
+                {
+                    Values = new double[] { 10,20,100,500 },
+                    Fill = null
+                }
+            };
+    }
+}
