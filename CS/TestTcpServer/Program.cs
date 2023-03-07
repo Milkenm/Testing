72ac7657d5d75822af463876337ed24830c38875
Milkenm <miguel.campos.johnny@gmail.com>
2023-03-07 15:22:07 +0000

    ScriptsLib test TCP server & client

---
+++ CS/TestTcpServer/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+using ScriptsLibV2;
+using ScriptsLibV2.Extensions;
+
+namespace TestTcp
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			TcpServer server = new TcpServer(666);
+			server.OnClientConnect += Server_OnClientConnect;
+			server.OnClientDisconnect += Server_OnClientDisconnect;
+			server.UseAsynchronousEvents = true;
+			server.Start();
+
+			Task.Delay(-1).Wait();
+		}
+
+		private static void Server_OnClientDisconnect(Socket client)
+		{
+			Console.WriteLine("Client disconnected.");
+		}
+
+		private static void Server_OnClientConnect(Socket client)
+		{
+			Console.WriteLine("Client connected.");
+			client.Send("Yup".ToByteArray());
+		}
+	}
+}
