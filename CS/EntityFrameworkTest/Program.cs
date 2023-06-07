571a6f830f5d4cb13e7647a62c5e29ae5b7cf519
Milkenm <miguel.campos.johnny@gmail.com>
2023-06-07 16:20:54 +0100

    EntityFramework test

---
+++ CS/EntityFrameworkTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using EntityFrameworkTest.Data;
+using EntityFrameworkTest.Models;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		using DatabaseContext context = new DatabaseContext();
+
+		Product p1 = new Product()
+		{
+			Name = "Veggie",
+			Price = 9.99M
+		};
+		context.Products.Add(p1);
+
+		Product p2 = new Product()
+		{
+			Name = "Deluxe",
+			Price = 12.99M,
+		};
+		context.Add(p2);
+
+		context.SaveChanges();
+	}
+}
